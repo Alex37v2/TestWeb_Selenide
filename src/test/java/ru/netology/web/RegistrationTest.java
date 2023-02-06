b2c82b071664b379b3c8c941b81d012c99e5c47f
Алексей Александров <Alex37v2@gmail.com>
2023-02-06 10:45:40 +1000

    fixes test mustRegisterAnAccount

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,8 +1,12 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -11,17 +15,25 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationTest {
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void mustRegisterAnAccount() {
         open("http://localhost:9999");
+        String planningDate = generateDate(4);
         $("span[data-test-id='city'] input").setValue("Владивосток");
-        $("span[data-test-id='date'] input").setValue("04.02.2023");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("span[data-test-id='name'] input").setValue("Кристина Кактус-Песикова");
         $("span[data-test-id='phone'] input").setValue("+79200000000");
         $("label[data-test-id='agreement']").click();
         $x("//span[@class='button__text']").click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $(byText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
-        $(byText("04.02.2023")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
